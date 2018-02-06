4038e1a8675843f016c9218f7a81b1112e5e0015
sigolg <wee8484@gmail.com>
2018-02-06 10:26:07 +0900

    mqtt

--- dht22_mqtt.lua
+++ dht22_mqtt.lua
@@ -1,17 +1,17 @@
 pin = 5 -- dht11 signal pin
 channelID = "409502"
 writeKey = "WKMTV2VSMBR2XU7P"
-time_between_sensor_readings = 10*1000*1000 --60000 means 60sec
+time_between_sensor_readings = 30*1000*1000 --60000 means 60sec
 
 --- MQTT ---
 m = mqtt.Client(channelID, 120)
- 
+
 --read DHT11 temp, humi data function
 function readDHT()
     status, temp, humi = dht.read(pin)
     if status == dht.OK then
-        print(temp)
-        print(humi)
+        print("Temperature: "..temp.."C")
+        print("Humidity: "..humi.."%")
     elseif status == dht.ERROR_CHECKSUM then
         print( "DHT Checksum error." )
     elseif status == dht.ERROR_TIMEOUT then
@@ -27,10 +27,11 @@ function sendData(temp,humi)
     m:connect( "mqtt.thingspeak.com" , 1883, 0, function(client)
         print("Connected to MQTT")
         print("  IP: mqtt.thingspeak.com")
-        print("  Port: 1883")
-        client:publish("channels/"..channelID.."/publish/"..writeKey,"field5="..temp.."&field6="..humi,0,0,functuin(client))
+        print("  Port:  1883")
+        client:publish("channels/"..channelID.."/publish/"..writeKey,"field5="..temp.."&field6="..humi, 0,0,function(client)
             print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
-            node.dsleep(time_between_sensor_readings)          
+            node.dsleep(time_between_sensor_readings)
+        end)
     end)
 end
 
