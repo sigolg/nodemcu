228c34292268f67d6d60e3dbd3b86394d09d9a97
sigolg <wee8484@gmail.com>
2018-02-07 10:35:39 +0900

    변수 최소화

--- init.lua
+++ init.lua
@@ -1,18 +1,5 @@
--- 4F unicorn info
---SSID="sigolCouple"
---PASSWORD="96239623"
---client_ip="172.30.1.101"     -- 101부터 순차적으로 사용
---client_netmask="255.255.255.0"
---client_gateway="172.30.1.254"
 -- 6F egg info
-SSID="ktEgg_095"
-PASSWORD="moda29379"
 client_ip="192.168.1.101"    -- 101부터 순차적으로 사용 
-client_netmask="255.255.255.0"
-client_gateway="192.168.1.1"
---client_ip=""
---client_netmask=""
---client_gateway=""
 
 function startup()
     if file.open("init.lua") == nil then
@@ -29,10 +16,10 @@ end
 print("Connecting to WiFi access point...")
 wifi.setmode(wifi.STATION)
 wifi.setphymode(wifi.PHYMODE_N)
-wifi.sta.config(SSID, PASSWORD)
+wifi.sta.config("ktEgg_095", "moda29379")
 wifi.sta.connect()
 if client_ip ~= "" then
-    wifi.sta.setip({ip=client_ip,netmask=client_netmask,gateway=client_gateway})
+    wifi.sta.setip({ip=client_ip,netmask="255.255.255.0",gateway="192.168.1.1"})
 end
 
 tmr.alarm(1, 1000, 1, function()
