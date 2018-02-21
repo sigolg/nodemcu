28f9f8b9c7724c2db950f129e005f56978fc5e97
sigolg <wee8484@gmail.com>
2018-02-21 09:58:53 +0900

    ok

--- dht22_mqtt.lua
+++ dht22_mqtt.lua
@@ -1,7 +1,7 @@
 pin = 5 -- dht11 signal pin
 channelID = ""
 writeKey = ""
-time_between_sensor_readings = 1800*1000*1000 --60000 means 60sec
+time_between_sensor_readings = 20*1000*1000 --60000 means 60sec
 
 --- MQTT ---
 m = mqtt.Client(channelID, 120)
@@ -23,8 +23,8 @@ end
 --send data to thingspeak
 function sendData(temp,humi)
     if temp == -999 then
-        print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
-        node.dsleep(time_between_sensor_readings)
+        print("Going to deep sleep for 10 seconds")
+        node.dsleep(10*1000*1000)
     else
         -- conection to thingspeak.com
         print("Sending data to thingspeak.com")
@@ -33,17 +33,18 @@ function sendData(temp,humi)
             print("  IP: mqtt.thingspeak.com")
             print("  Port: 1883")
             client:publish("channels/"..channelID.."/publish/"..writeKey,"field1="..temp.."&field2="..humi,0,0,function(client)
-                print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
-                node.dsleep(time_between_sensor_readings)    
+                client:close()
+                print("Going to stay for "..(time_between_sensor_readings/1000000).." seconds")  
             end)
         end,
         function(client, reason)
             print("failed reason: " .. reason)
-            print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
-            node.dsleep(time_between_sensor_readings) 
+            print("Going to deep sleep for 10 seconds")
+            node.dsleep(10*1000*1000) 
         end)
     )
 end
 
 readDHT()
 sendData(temp,humi)  
+tmr.alarm(1,time_between_sensor_readings/1000,1,function() readDHT()sendData(temp,humi) end)
