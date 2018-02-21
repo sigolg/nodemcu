b8c78ab0529e3d9b1d7f5ce242e8360409588571
sigolg <wee8484@gmail.com>
2018-02-21 09:36:05 +0900

    final

--- dht22_mqtt.lua
+++ dht22_mqtt.lua
@@ -1,7 +1,7 @@
 pin = 5 -- dht11 signal pin
 
-channelID = "409502"
-writeKey = "WKMTV2VSMBR2XU7P"
+channelID = ""
+writeKey = ""
 time_between_sensor_readings = 180*1000*1000 --60000 means 60sec
 
 --- MQTT ---
@@ -15,12 +15,8 @@ function readDHT()
         print("Humi: "..humi.."%")
     elseif status == dht.ERROR_CHECKSUM then
         print( "DHT Checksum error." )
-        print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
-        node.dsleep(time_between_sensor_readings)
     elseif status == dht.ERROR_TIMEOUT then
         print( "DHT timed out." )
-        print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
-        node.dsleep(time_between_sensor_readings)
     end
     dht = nil
 end
@@ -28,22 +24,27 @@ end
 --send data to thingspeak
 function sendData(temp,humi)
     -- conection to thingspeak.com
-    print("Sending data to thingspeak.com")
-    m:connect( "mqtt.thingspeak.com" , 1883, 0, function(client)
-        print("Connected to MQTT")
-        print("  IP: mqtt.thingspeak.com")
-        print("  Port: 1883")
-        client:publish("channels/"..channelID.."/publish/"..writeKey,"field3="..temp.."&field4="..humi,0,0,function(client)
-            client:close()
-            print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
-            node.dsleep(time_between_sensor_readings)
+    if temp == -999 then
+        print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
+        node.dsleep(time_between_sensor_readings)
+    else
+        print("Sending data to thingspeak.com")
+        m:connect( "mqtt.thingspeak.com" , 1883, 0, function(client)
+            print("Connected to MQTT")
+            print("  IP: mqtt.thingspeak.com")
+            print("  Port: 1883")
+            client:publish("channels/"..channelID.."/publish/"..writeKey,"field3="..temp.."&field4="..humi,0,0,function(client)
+                client:close()
+                print("Going to deep sleep for "..(time_between_sensor_readings/1000000).." seconds")
+                node.dsleep(time_between_sensor_readings)
+            end)
+        end,
+        function(client, reason)
+                print("failed reason: " .. reason)
+                print("Going to deep sleep for 60 seconds")
+                node.dsleep(time_between_sensor_readings)
         end)
-    end,
-    function(client, reason)
-            print("failed reason: " .. reason)
-            print("Going to deep sleep for 60 seconds")
-            node.dsleep(time_between_sensor_readings)
-    end)
+    end
 end
 
 readDHT()
